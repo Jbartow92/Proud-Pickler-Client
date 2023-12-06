6fa4182a555915ab77dcb3e8d37d8eb298472a3d
Josh Bartow <jbartow92@gmail.com>
2023-12-06 13:30:34 -0600

    pp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export const Home = () => {
     return (
       <main className='welcome-container'>
-        <h1 className='welcome-message'>Welcome to Proud Picklers</h1>
+        <h1 className='welcome-message'>WELCOME TO PROUD PICKLERS</h1>
       </main>
     )
   }
