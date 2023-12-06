445cae6cbca41439a45fb60f8290cb41c05f50db
Josh Bartow <jbartow92@gmail.com>
2023-12-06 11:57:20 -0600

    first client commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+    return (
+      <main className='welcome-container'>
+        <h1 className='welcome-message'>Welcome to Proud Picklers</h1>
+      </main>
+    )
+  }
+  
